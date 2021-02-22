8c830197b54365dde8cf3b3b0ed9a36c0ae31774
Jojo Theerapat <jojo.theerapat@hotmail.com>
2021-02-23 04:41:13 +0700

    add more endpoints

--- pokedex-client/app/services/user.service.ts
+++ pokedex-client/app/services/user.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '@environments/environment';
+import { User } from '@models/user.model';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -10,8 +11,24 @@ export class UserService {
 
   constructor(private http: HttpClient) { }
 
-  getUser(user: any): Observable<any> {
-    return this.http.get<any>(`${environment.apiUrl}/user/${user._id}`);
+  getUser(user: User): Observable<User> {
+    return this.http.get<User>(`${environment.apiUrl}/users/${user._id}`);
+  }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(`${environment.apiUrl}/users`);
+  }
+
+  createUser(user: User): Observable<User> {
+    throw new Error('Method not implemented.');
+  }
+
+  updateUser(id: string, user: User): Observable<User> {
+    throw new Error('Method not implemented.');
+  }
+
+  deleteUser(id: string): Observable<User> {
+    throw new Error('Method not implemented.');
   }
 
 }
