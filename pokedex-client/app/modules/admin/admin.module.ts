5388d6ccc18a654730ec9aadea680152f17c24ea
Jojo Theerapat <jojo.theerapat@hotmail.com>
2021-02-27 19:11:44 +0700

    remove unused code

--- pokedex-client/app/modules/admin/admin.module.ts
+++ pokedex-client/app/modules/admin/admin.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { AccountMenuComponent } from '@components/account-menu/account-menu.component';
 import { AdminComponent } from '@components/admin/admin.component';
 import { ColumnFilterComponent } from '@components/column-filter/column-filter.component';
 import { ManageTrainersComponent } from '@components/manage-trainers/manage-trainers.component';
