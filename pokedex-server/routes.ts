7b0e3a708ead30d691e9a59605f2363bb5e2c7ae
Jojo Theerapat <jojo.theerapat@hotmail.com>
2021-02-28 22:23:19 +0700

    protect the resource by role

--- pokedex-server/routes.ts
+++ pokedex-server/routes.ts
@@ -2,6 +2,7 @@ import * as express from 'express';
 import * as jwt from 'express-jwt';
 import RoleController from './controllers/role.controller';
 import UserController from './controllers/user.controller';
+import createError = require('http-errors');
 
 function setRoutes(app: any): void {
   const userCtrl = new UserController();
@@ -13,7 +14,10 @@ function setRoutes(app: any): void {
   app.use('/auth', authRouter);
 
   const apiRouter = express.Router();
-  apiRouter.use(jwt({ secret: process.env.SECRET_ACCESS_TOKEN as string, algorithms: ['HS256'] }));
+  apiRouter.use(
+    jwt({ secret: process.env.SECRET_ACCESS_TOKEN as string, algorithms: ['HS256'] }),
+    checkIfAdminRole
+  );
   apiRouter.route('/users').get(userCtrl.getAll);
   apiRouter.route('/users').post(userCtrl.insert);
   apiRouter.route('/users/count').get(userCtrl.count);
@@ -31,6 +35,14 @@ function setRoutes(app: any): void {
     .put(roleCtrl.update)
     .delete(roleCtrl.delete);
   app.use('/api/v1', apiRouter);
+}
+
+function checkIfAdminRole(req: any, res: any, next: any) {
+  const role = req.user.user.role.text.toLowerCase();
+  if (role !== 'admin') {
+    return next(new createError.Forbidden('Only admin is allowed to access the resource'));
+  }
+  next();
 }
 
 export default setRoutes;
